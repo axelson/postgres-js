2c3468e451034b283b69e5ec5d48c06db36c3467
Jason Axelson <jason.axelson@gmail.com>
2010-10-02 12:41:39 -1000

    Nicer demo

--- demo.js
+++ demo.js
@@ -2,19 +2,32 @@ var sys = require("sys");
 var pg = require("./lib/postgres-pure");
 pg.DEBUG=0;
 
-var db = new pg.connect("pgsql://test:12345@localhost:5432/template1");
-db.prepare("SELECT ?::int", function (sth) {
-    sth.execute([1], function (rs) {
-        for (var i = 0; i < rs.length; i++) {
-            for (var key in rs[i]) {
-                if (rs[i].hasOwnProperty(key)) {
-                    sys.puts(key +": " +rs[i][key]);
-                }
-            }
-        }
-        db.end();
-    });
-    sth.execute([2], function (rs) {
-        sys.puts(sys.inspect(rs));
-    })
+console.log("hi");
+var db = new pg.connect("pgsql://amino:1@localhost:5432/template1");
+console.log("hi2");
+
+db.query("SELECT * FROM test5", function (data) {
+    //sys.p(data);
+    console.log("hiin");
 });
+console.log("hi3");
+db.end();
+//db.close();
+console.log("hi4");
+
+
+//db.prepare("SELECT ?::int", function (sth) {
+//    sth.execute([1], function (rs) {
+//        for (var i = 0; i < rs.length; i++) {
+//            for (var key in rs[i]) {
+//                if (rs[i].hasOwnProperty(key)) {
+//                    sys.puts(key +": " +rs[i][key]);
+//                }
+//            }
+//        }
+//        db.end();
+//    });
+//    sth.execute([2], function (rs) {
+//        sys.puts(sys.inspect(rs));
+//    })
+//});
